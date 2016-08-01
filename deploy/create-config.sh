6ff5b0d3a1d7926fc38ecefb0f20149186c7b33f
Aaron Roydhouse <aaron@roydhouse.com>
2016-08-01 13:55:57 -0400

    Add option to pass authorized_keys file to use for config as a parameter

--- deploy/create-config.sh
+++ deploy/create-config.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
+
 CONFIGMAP_NAME=octopus-kubectl
+
+AUTHORIZED_KEYS_FILE=authorized_keys
+if [ "$1" != "" ]; then
+  AUTHORIZED_KEYS_FILE=$1
+fi
+echo Using authorized_keys from ${AUTHORIZED_KEYS_FILE}
+
 kubectl delete configmap --ignore-not-found $CONFIGMAP_NAME
-kubectl create configmap $CONFIGMAP_NAME --from-file=authorized-keys=authorized_keys
+kubectl create configmap $CONFIGMAP_NAME --from-file=authorized-keys=${AUTHORIZED_KEYS_FILE}
 kubectl label configmap $CONFIGMAP_NAME app=octopus-kubectl
