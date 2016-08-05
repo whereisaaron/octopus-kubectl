7d623bae4c56dcdfe86dd746487d3155aae80f89
Aaron Roydhouse <aaron@roydhouse.com>
2016-08-05 13:26:06 -0400

    Add option to create-config.sh to provide authorized_keys file path as an argument Update Dockerfile comments

--- deploy/create-config.sh
+++ deploy/create-config.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+# Create a configMap with ssh public keys
+#
+# Note that if you update the configMap you do not need to restart the pod,
+# the mapped authorized_keys file will be updated automatically
+#
+# You can also pass a file name: create-config.sh /path/to/my_authorized_keys_file
+#
+
+# Exit on first error
 set -e
 
 CONFIGMAP_NAME=octopus-kubectl
 
+# Used authorized_keys in this folder unless a file is specified
 AUTHORIZED_KEYS_FILE=authorized_keys
 if [ "$1" != "" ]; then
   AUTHORIZED_KEYS_FILE=$1
