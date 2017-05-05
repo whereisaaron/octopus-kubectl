693393a383943e6edf366bb706088bd59ddd0e5f
Aaron Roydhouse <aaron@roydhouse.com>
2017-05-05 12:34:41 -0400

    Add deployment support for HELM_HOST environment variable Update startup.sh script to copy HELM_ environment variables into root profile for ssh sessions Add annotation k8s 1.5.x toleration for spot instances

--- build/start.sh
+++ build/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #
-# Copy Kubernetes API environment variables to root profile,
-# so the environment variables are available to kubectl
-# when Octopus uses ssh
+# Copy Kubernetes API and Helm environment variables to root
+# profile, so the environment variables are available to
+# kubectl when Octopus uses ssh
 #
 # If the container is not running in Kubernetes or you want
 # to deploy to a remote Kubernetes cluster then you should
 # mount a /root/.kube/config file to enable access
 #
 
-env | grep KUBERNETES_ | sed -e 's/^/export /' >> /root/.profile
+env | grep '^\(KUBERNETES_\|HELM_\)' | sed -e 's/^/export /' >> /root/.profile
 
 #
 # Start sshd unless an alternative command supplied
--- deploy/Deploy.sh
+++ deploy/Deploy.sh
@@ -9,4 +9,7 @@ export SERVICEACCOUNT=${SERVICEACCOUNT:-default}
 # Optionally you can set the fixed NodePort to use
 export NODEPORT=${NODEPORT:-31800}
 
+# Optionally you can set the helm tiller service domain name to use
+export HELM_HOST=${HELM_HOST:-tiller-deploy.kube-system.svc.cluster.local:44134}
+
 envsubst < octopus-kubectl-template.yaml | kubectl apply -f -
