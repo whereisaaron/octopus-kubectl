dcb489515173a5b1049b078b8923fcadd8b21d5b
Aaron Roydhouse <aaron@roydhouse.com>
2017-05-06 22:50:35 -0400

    Add Secrets to Display.sh

--- deploy/Display.sh
+++ deploy/Display.sh
@@ -3,4 +3,4 @@
 # Optionally you can set a custom name space
 export NAMESPACE=${NAMESPACE:-default}
 
-kubectl get svc,pod,cm --selector app=octopus-kubectl --namespace=${NAMESPACE}
+kubectl get svc,pod,cm,secret --selector app=octopus-kubectl --namespace=${NAMESPACE}
